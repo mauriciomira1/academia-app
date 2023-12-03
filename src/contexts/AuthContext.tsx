22e0747034570e1059c51b86395bdbee9643a40a
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-12-03 11:15:13 -0300

    feat: inserindo função eject na api

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -114,6 +114,14 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     loadUserData();
   }, []);
 
+  useEffect(() => {
+    const subscribe = api.registerInterceptTokenManager(signOut);
+
+    return () => {
+      subscribe();
+    };
+  }, [signOut]);
+
   return (
     <AuthContext.Provider
       value={{
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,18 +1,29 @@
 import { AppError } from "@utils/AppError";
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+
+type SignOut = () => void;
+
+type APIIntanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void;
+};
 
 const api = axios.create({
   baseURL: "http://192.168.3.2:3333",
-});
+}) as APIIntanceProps;
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.data) {
-      return Promise.reject(new AppError(error.response.data.message));
-    } else {
-      return Promise.reject(error);
+api.registerInterceptTokenManager = (signOut) => {
+  const interceptTokenManager = api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response && error.response.data) {
+        return Promise.reject(new AppError(error.response.data.message));
+      } else {
+        return Promise.reject(error);
+      }
     }
-  }
-);
+  );
+
+  return () => api.interceptors.response.eject(interceptTokenManager);
+};
+
 export { api };
